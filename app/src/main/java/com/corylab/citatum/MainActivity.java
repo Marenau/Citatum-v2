70d59db496505516e23b32ef57343d7d6ced665a
Vadim <malin.vadimka@mail.ru>
2023-02-20 23:42:00 +0300

    Add initialization and some temp items

--- app/src/main/java/com/corylab/citatum/MainActivity.java
+++ app/src/main/java/com/corylab/citatum/MainActivity.java
@@ -3,6 +3,8 @@ package com.corylab.citatum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initIt();
+    }
+
+    private void initIt() {
+        TextView pageText = findViewById(R.id.page_name);
+        pageText.setText(R.string.home_app_text);
+
+        TextView welcomeText = findViewById(R.id.welcome_text);
+        welcomeText.setText(R.string.welcome_day_text);
+
+        TextView dateText = findViewById(R.id.date_text);
+        dateText.setText(R.string.temp_date);
+
+        TextView tagsText = findViewById(R.id.tags_text);
+        tagsText.setText(R.string.tags_text);
+
+        ImageView image = findViewById(R.id.image);
+        image.setImageResource(R.drawable.colors);
     }
 }
