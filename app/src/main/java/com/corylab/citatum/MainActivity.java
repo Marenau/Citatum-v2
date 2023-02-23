cd3bb662fa3d14612b30b0946592afc378c38e17
Vadim <malin.vadimka@mail.ru>
2023-02-23 13:29:22 +0300

    Add RepositoryActivity

--- app/src/main/java/com/corylab/citatum/MainActivity.java
+++ app/src/main/java/com/corylab/citatum/MainActivity.java
@@ -1,13 +1,12 @@
 package com.corylab.citatum;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.corylab.citatum.databinding.ActivityMainBinding;
 
@@ -28,13 +27,28 @@ public class MainActivity extends AppCompatActivity {
     private void initIt() {
         binding.pageName.setText(R.string.home_app_text);
         binding.welcomeText.setText(R.string.welcome_day_text);
-        binding.dateText.setText(R.string.temp_date);
-        binding.tagsText.setText(R.string.tags_text);
+        binding.dateText.setText(R.string.current_date);
+        binding.transmittedText.setText(R.string.default_text);
         binding.quoteEditText.setHint(R.string.write_text_request);
 
-        binding.addButton.setText(R.string.add_button);
-        binding.addButton.setOnClickListener(view -> Log.i("Citatum", "Button is pressed!"));
+        binding.intentButton.setText(R.string.add_button);
 
         binding.image.setImageResource(R.drawable.colors);
+    }
+
+    public void onButtonClick(View view) {
+        Log.i("Citatum", "Button is pressed!");
+        Intent intent = new Intent(this, RepositoryActivity.class);
+        intent.putExtra("Text", "Вторая активность");
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String resultText = data.getStringExtra("resultText");
+            binding.transmittedText.setText(resultText);
+        }
     }
 }
