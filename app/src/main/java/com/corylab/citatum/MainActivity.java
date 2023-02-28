da51c0cd5d41bd6d350827dc2d056d1014b2d89e
Vadim <malin.vadimka@mail.ru>
2023-02-28 18:03:34 +0300

    Remove RepositoryActivity add Fragments

--- app/src/main/java/com/corylab/citatum/MainActivity.java
+++ app/src/main/java/com/corylab/citatum/MainActivity.java
@@ -12,43 +12,9 @@ import com.corylab.citatum.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        initIt();
-    }
-
-    private void initIt() {
-        binding.pageName.setText(R.string.home_app_text);
-        binding.welcomeText.setText(R.string.welcome_day_text);
-        binding.dateText.setText(R.string.current_date);
-        binding.transmittedText.setText(R.string.default_text);
-        binding.quoteEditText.setHint(R.string.write_text_request);
-
-        binding.intentButton.setText(R.string.add_button);
-
-        binding.image.setImageResource(R.drawable.colors);
-    }
-
-    public void onButtonClick(View view) {
-        Log.i("Citatum", "Button is pressed!");
-        Intent intent = new Intent(this, RepositoryActivity.class);
-        intent.putExtra("Text", binding.quoteEditText.getText().toString());
-        startActivityForResult(intent, 1);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            String resultText = data.getStringExtra("resultText");
-            binding.transmittedText.setText(resultText);
-        }
+        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/corylab/citatum/fragment/HubFragment.java
@@ -0,0 +1,12 @@
+package com.corylab.citatum.fragment;
+
+import androidx.fragment.app.Fragment;
+
+import com.corylab.citatum.R;
+
+public class HubFragment extends Fragment {
+
+    public HubFragment() {
+        super(R.layout.hub_fragment);
+    }
+}
