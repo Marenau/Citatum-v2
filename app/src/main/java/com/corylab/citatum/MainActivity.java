414161236ee1949e43c2854c69250efe141b1e8c
Vadim <malin.vadimka@mail.ru>
2023-03-02 22:05:20 +0300

    Add Log and Toast

--- app/src/main/java/com/corylab/citatum/MainActivity.java
+++ app/src/main/java/com/corylab/citatum/MainActivity.java
@@ -2,6 +2,7 @@ package com.corylab.citatum;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,12 +10,27 @@ import android.util.Log;
 import android.view.View;
 
 import com.corylab.citatum.databinding.ActivityMainBinding;
+import com.corylab.citatum.fragment.BottomNavigationFragment;
+import com.corylab.citatum.fragment.HubFragment;
+import com.corylab.citatum.fragment.LeftNavigationFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null)
+            initFragments();
+    }
+
+    private void initFragments() {
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().setReorderingAllowed(true).add(R.id.main_fragment_container_view, HubFragment.class, null).commit();
+        fragmentManager.beginTransaction().setReorderingAllowed(true).add(R.id.bottom_fragment_container_view, BottomNavigationFragment.class, null).commit();
+        fragmentManager.beginTransaction().setReorderingAllowed(true).add(R.id.left_fragment_container_view, LeftNavigationFragment.class, null).commit();
     }
 }
--- app/src/main/java/com/corylab/citatum/fragment/HubFragment.java
+++ app/src/main/java/com/corylab/citatum/fragment/HubFragment.java
@@ -1,5 +1,15 @@
 package com.corylab.citatum.fragment;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.corylab.citatum.R;
@@ -8,5 +18,89 @@ public class HubFragment extends Fragment {
 
     public HubFragment() {
         super(R.layout.hub_fragment);
+    }
+
+    private void showToast(String text) {
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(getContext(), text, duration);
+        toast.show();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        showToast("onCreate");
+        Log.i("Citatum", "onCreate");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        showToast("onCreateView");
+        Log.i("Citatum", "onCreateView");
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        showToast("onViewCreate");
+        Log.i("Citatum", "onViewCreate");
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        showToast("onViewStateRestored");
+        Log.i("Citatum", "onViewStateRestored");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        showToast("onStart");
+        Log.i("Citatum", "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        showToast("onResume");
+        Log.i("Citatum", "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        showToast("onPause");
+        Log.i("Citatum", "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        showToast("onStop");
+        Log.i("Citatum", "onStop");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        showToast("onSaveInstanceState");
+        Log.i("Citatum", "onSaveInstanceState");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        showToast("onDestroyView");
+        Log.i("Citatum", "onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        showToast("onDestroy");
+        Log.i("Citatum", "onDestroy");
     }
 }
